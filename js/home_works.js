95db871393c84ffce6b572178d8d60a288b6ff37
makarov <25.makarovv@gmail.com>
2023-11-30 10:46:24 +0600

    update project

--- js/home_works.js
+++ js/home_works.js
@@ -1,107 +1,111 @@
-var gmailInput = document.getElementById('gmail_input');
-var gmailButton = document.getElementById('gmail_button');
-var gmailResult = document.getElementById('gmail_result');
-const btnStart = document.getElementById("start");
-const btnStop = document.getElementById("stop");
-const btnReset = document.getElementById("reset");
-
-const divMlSeconds = document.getElementById("ml-secondsS");
-const divSeconds = document.getElementById("secondsS");
-const divMinutes = document.getElementById("minutesS");
-
-var mlSeconds = 0;
-var seconds = 0;
-
-gmailButton.addEventListener('click', function () {
-    var enteredEmail = gmailInput.value.trim();
-
-    // Регулярное выражение для проверки, что email - это gmail
-    var gmailRegex = /^[a-zA-Z0-9_.+-]+@gmail\.com$/;
-
-    // Проверяем, что email - это gmail
-    if (validateGmail(enteredEmail, gmailRegex)) {
-        gmailResult.textContent = 'GMAIL!';
-        gmailResult.style.color = 'green';
-    } else {
-        gmailResult.textContent = 'Not Gmail...';
-        gmailResult.style.color = 'red';
-    }
-});
-
-function validateGmail(email, regex) {
-    if (regex.test(email)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function dvizhenieIUUUU() {
-    const childBlock = document.querySelector('.child_block');
+window.onload = function () {
     const parentBlock = document.querySelector('.parent_block');
+    const childBlock = document.querySelector('.child_block');
+
+    let posX = 0;
+    let posY = 0;
+
     const parentWidth = parentBlock.offsetWidth;
     const parentHeight = parentBlock.offsetHeight;
-    const childWidth = childBlock.offsetWidth;
-    const childHeight = childBlock.offsetHeight;
 
-    let top = 0;
-    let left = 0;
-    let moveRight = true;
 
-    const moveInterval = setInterval(() => {
-        if (moveRight) {
-            if (left + childWidth >= parentWidth) {
-                moveRight = false;
+    function moveRight() {
+        const interval = setInterval(() => {
+            if (posX >= parentWidth - childBlock.offsetWidth) {
+                clearInterval(interval);
+                moveDown();
             } else {
-                left += 5;
+                posX += 1;
+                childBlock.style.left = posX + 'px';
             }
+        }, 10);
+    }
+
+
+    function moveDown() {
+        const interval = setInterval(() => {
+            if (posY >= parentHeight - childBlock.offsetHeight) {
+                clearInterval(interval);
+            } else {
+                posY += 1;
+                childBlock.style.top = posY + 'px';
+            }
+        }, 10);
+    }
+
+    childBlock.style.left = posX + 'px';
+    childBlock.style.top = posY + 'px';
+
+    setTimeout(() => {
+        moveRight();
+    }, 1000);
+
+    const gmailInput = document.getElementById('gmail_input');
+    const gmailButton = document.getElementById('gmail_button');
+    const gmailResult = document.getElementById('gmail_result');
+
+    gmailButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const enteredEmail = gmailInput.value.trim();
+
+        const validGmail = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)*@gmail\.com$/;
+
+        if (validGmail.test(enteredEmail)) {
+            gmailResult.textContent = 'правильный';
         } else {
-            if (top + childHeight >= parentHeight) {
-                clearInterval(moveInterval);
-            } else {
-                top += 5;
-            }
+            gmailResult.textContent = 'неправильный';
         }
+    });
 
-        childBlock.style.top = top + 'px';
-        childBlock.style.left = left + 'px';
-    }, 30);
-}
 
-dvizhenieIUUUU();
 
-let timer = null;
 
-function startTimer() {
-    if (!timer) {
-        timer = setInterval(updateTimer, 10);
+    const btnStart = document.getElementById("start");
+    const btnStop= document.getElementById("stop");
+    const btnReset= document.getElementById("reset");
+
+    const divMlSeconds = document.getElementById("ml-secondsS");
+    const divSeconds= document.getElementById("secondsS");
+
+
+    var mlSeconds = 0;
+    var seconds= 0;
+
+
+    let timer = null;
+
+    function startTimer() {
+        if (!timer) {
+            timer = setInterval(updateTimer, 10); // Запуск таймера с интервалом 10 миллисекунд (для миллисекунд)
+        }
     }
-}
 
-function updateTimer() {
-    mlSeconds++;
-    if (mlSeconds === 100) {
+    function updateTimer() {
+        mlSeconds++;
+        if (mlSeconds === 100) {
+            mlSeconds = 0;
+            seconds++;
+        }
+        divMlSeconds.textContent = mlSeconds < 10 ? `0${mlSeconds}` : mlSeconds;
+        divSeconds.textContent = seconds < 10 ? `0${seconds}` : seconds;
+    }
+
+    function stopTimer() {
+        clearInterval(timer); // Остановка интервала
+        timer = null;
+    }
+
+    function resetTimer() {
+        clearInterval(timer);
         mlSeconds = 0;
-        seconds++;
+        seconds = 0;
+        divMlSeconds.textContent = "00";
+        divSeconds.textContent = "00";
+        timer = null;
     }
-    divMlSeconds.textContent = mlSeconds < 10 ? 0${mlSeconds} : mlSeconds;
-    divSeconds.textContent = seconds < 10 ? 0${seconds} : seconds;
-}
 
-function stopTimer() {
-    clearInterval(timer);
-    timer = null;
-}
-
-function resetTimer() {
-    clearInterval(timer);
-    mlSeconds = 0;
-    seconds = 0;
-    divMlSeconds.textContent = "00";
-    divSeconds.textContent = "00";
-    timer = null;
-}
-
-btnStart.addEventListener('click', startTimer);
-btnStop.addEventListener('click', stopTimer);
-btnReset.addEventListener('click', resetTimer);
+    btnStart.addEventListener('click', startTimer);
+    btnStop.addEventListener('click', stopTimer);
+    btnReset.addEventListener('click', resetTimer);
+};
